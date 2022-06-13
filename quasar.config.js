b1747566fa7d67b60400520257e9c1a7e8e291ef
Nasir Lawal <nasirlawal001@gmail.com>
2022-06-13 19:44:03 +0100

    add vercel conf

--- quasar.config.js
+++ quasar.config.js
@@ -77,8 +77,8 @@ module.exports = configure(function (ctx) {
       chainWebpack (chain) {
         chain.plugin('eslint-webpack-plugin')
           .use(ESLintPlugin, [{ extensions: [ 'js', 'vue' ] }])
-      }
-      
+      },
+      distDir: ctx.mode.spa ? 'dist' : null,
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-devServer
