2f7bea7a859e5c5b53150bbb32dfa4e1bfc22836
Nasir Lawal <nasirlawal001@gmail.com>
2022-06-14 17:28:09 +0100

    add tablet responsive to content section

--- src/components/landing/ContentSection.vue
+++ src/components/landing/ContentSection.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="tw-mt-44">
-    <div class="tw-flex tw-justify-center bg-accent tw-h-80">
+    <div class="tw-flex tw-justify-center bg-accent tw-h-64 lg:tw-h-80">
       <div class="">
         <q-img
-          class="tw-w-120 -tw-mt-48 step"
+          class="tw-w-96 lg:tw-w-120 md:-tw-mt-32 lg:-tw-mt-48 step"
           src="~assets/img/landing/hero.png"
         />
       </div>
-      <div class="tw-pt-16  tw-border-l-2 tw-border-white tw-pl-12">
-        <div class="tw-text-4xl tw-font-extrabold tw-text-white">
+      <div class="tw-pt-5 lg:tw-pt-16  tw-border-l-2 tw-border-white tw-pl-12">
+        <div class="tw-text-2xl lg:tw-text-4xl tw-font-extrabold tw-text-white">
           Personalized training <br>
           and content
         </div>
-        <div class="tw-pt-8 tw-text-white">
-          Our content approach gives curisty and help set strengths, and interests in what <br>
-          you are learning, this helps us in highlighting your abilities rather than iluminating <br>
-          your disabilties. navigating through the learning phase is challanging we provide <br>
+        <div class="tw-pt-4 lg:tw-pt-8 tw-text-white">
+          Our content approach gives curisty and help set strengths, and interests in what <br class="tw-hidden lg:tw-block">
+          you are learning, this helps us in highlighting your abilities rather than iluminating <br class="tw-hidden lg:tw-block">
+          your disabilties. navigating through the learning phase is challanging we provide <br class="tw-hidden lg:tw-block">
           you the best support to grow with adaptive apraches
         </div>
       </div>
