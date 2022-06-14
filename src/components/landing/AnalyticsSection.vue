ffa6ee87b400ef3b53d9ae9f0e990d9809bd3013
Nasir Lawal <nasirlawal001@gmail.com>
2022-06-14 21:45:20 +0100

    responsive analytics section

--- src/components/landing/AnalyticsSection.vue
+++ src/components/landing/AnalyticsSection.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="tw-mt-32">
-    <div class="tw-flex tw-justify-center tw-mx-44">
+    <div class="tw-flex tw-justify-center sm:tw-mx-44">
       <div class="lg:tw-mr-44">
-        <div class="tw-flex">
-          <div class="tw-bg-primaryColor tw-rounded-md tw-text-center tw-mx-2 tw-text-white tw-cursor-pointer hover:tw-w-36 hover:tw-h-36 tw-w-32 tw-h-32">
+        <div class="tw-flex tw-justify-center">
+          <div class="tw-bg-primaryColor tw-rounded-md tw-text-center tw-mx-2 tw-text-white tw-cursor-pointer hover:tw-w-36 hover:tw-h-36 tw-w-28 tw-h-28 sm:tw-w-32 sm:tw-h-32">
             <div class="tw-pt-9 tw-text-base">
               Students
             </div>
@@ -11,7 +11,7 @@
               1M+
             </div>
           </div>
-          <div class="tw-bg-primaryColor tw-rounded-md tw-text-center tw-mx-2 tw-text-white tw-cursor-pointer hover:tw-w-36 hover:tw-h-36 tw-w-32 tw-h-32">
+          <div class="tw-bg-primaryColor tw-rounded-md tw-text-center tw-mx-2 tw-text-white tw-cursor-pointer hover:tw-w-36 hover:tw-h-36 tw-w-28 tw-h-28 sm:tw-w-32 sm:tw-h-32">
             <div class="tw-pt-9 tw-text-base">
               Countries
             </div>
@@ -19,7 +19,7 @@
               12
             </div>
           </div>
-          <div class="tw-bg-primaryColor tw-rounded-md tw-text-center tw-mx-2 tw-text-white tw-cursor-pointer hover:tw-w-36 hover:tw-h-36 tw-w-32 tw-h-32">
+          <div class="tw-bg-primaryColor tw-rounded-md tw-text-center tw-mx-2 tw-text-white tw-cursor-pointer hover:tw-w-36 hover:tw-h-36 tw-w-28 tw-h-28 sm:tw-w-32 sm:tw-h-32">
             <div class="tw-pt-9 tw-text-base">
               Communities
             </div>
@@ -28,8 +28,8 @@
             </div>
           </div>
         </div>
-        <div class="tw-flex tw-mt-4">
-          <div class="tw-bg-primaryColor tw-rounded-md tw-text-center tw-mx-2 tw-text-white tw-cursor-pointer hover:tw-w-36 hover:tw-h-36 tw-w-32 tw-h-32">
+        <div class="tw-flex tw-justify-center tw-mt-4">
+          <div class="tw-bg-primaryColor tw-rounded-md tw-text-center tw-mx-2 tw-text-white tw-cursor-pointer hover:tw-w-36 hover:tw-h-36 tw-w-28 tw-h-28 sm:tw-w-32 sm:tw-h-32">
             <div class="tw-pt-9 tw-text-base">
               Languages
             </div>
@@ -37,7 +37,7 @@
               3
             </div>
           </div>
-          <div class="tw-bg-primaryColor tw-rounded-md tw-text-center tw-mx-2 tw-text-white tw-cursor-pointer hover:tw-w-36 hover:tw-h-36 tw-w-32 tw-h-32">
+          <div class="tw-bg-primaryColor tw-rounded-md tw-text-center tw-mx-2 tw-text-white tw-cursor-pointer hover:tw-w-36 hover:tw-h-36 tw-w-28 tw-h-28 sm:tw-w-32 sm:tw-h-32">
             <div class="tw-pt-9 tw-text-base">
               Partnerships
             </div>
@@ -45,16 +45,22 @@
               2
             </div>
           </div>
+          <div class="">
+            <q-img
+              class="tw-w-28 tw-ml-4 hover:tw-w-40 tw-cursor-pointer"
+              src="~assets/img/landing/logo2.png"
+            />
+          </div>
         </div>
-        <div class="tw-pt-6 tw-pl-2">
-          Resonating proficiency is what we are out for, we provide learners with <br>
+        <div class="tw-pt-6 tw-px-4 sm:tw-pl-2">
+          Resonating proficiency is what we are out for, we provide learners with <br class="tw-hidden sm:tw-block">
           mastery at their comfort zone. Anytime anywhere.
         </div>
-        <div class="tw-my-8 tw-ml-2">
+        <div class="tw-my-8 tw-mx-4 sm:tw-ml-2">
           <q-btn color="accent" label="Get started" no-caps unelevated class="tw-w-44 tw-h-14" @click="onClick" />
         </div>
       </div>
-      <div class="tw-mt-20 tw-ml-36">
+      <div class="tw-mt-20 tw-hidden sm:tw-block tw-ml-36">
         <q-img
           class="tw-w-36 hover:tw-w-40 tw-cursor-pointer"
           src="~assets/img/landing/logo2.png"
